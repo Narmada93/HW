3b716f5baa59170844d2cf789992f1824b3840d7
Narmada <Narmada.salluri77@gmail.com>
2024-01-28 23:30:19 +0530

    Palindrome

---
+++ src/oops_08_6th_Jan/Lab137.java
@@ -0,0 +1,30 @@
+package src.oops_08_6th_Jan;
+
+import java.util.Scanner;
+
+public class Lab137 {
+    public static void main(String[] args) {
+
+
+        // palindrome
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter a string : ");
+        String user_input = scanner.next();
+        boolean result = isPalindrome(user_input);
+        if(result){
+            System.out.println(result +"\n "+ user_input +" is a Palindrome");
+        }else{
+            System.out.println(result + " It is not a Palindrome");
+        }
+
+    }
+
+    private static boolean isPalindrome(String userInput) {
+
+        String original_str = userInput;
+        StringBuilder sb = new StringBuilder(userInput);
+        String reverse_string = sb.reverse().toString();
+        return original_str.equalsIgnoreCase(reverse_string);
+    }
+}
